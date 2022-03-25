a403acd949c8f52dd8ab59aef31f7050c710070a
pewphoria <101679444+pewphoria@users.noreply.github.com>
2022-03-25 17:48:10 -0500

    Update to js/css

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
-descriptionElement.innerHTML = (response.data.weather.description);
-cityElement.innerHTML = (response.data.main.city);
-temperatureElement.innerHTML = Math.round
-(response.data.main.temp);
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+windElement.innerHTML = Math.round(response.data.wind.speed);
+humidityElement.innerHTML = (response.data.main.humidity);
+descriptionElement.innerHTML = (response.data.weather[0].description);
+cityElement.innerHTML = (response.data.name);
+temperatureElement.innerHTML = Math.round(response.data.temp);
 }
 
 let apiKey = "f1ee79c9eeb6035ca48c765eafeb744e";
