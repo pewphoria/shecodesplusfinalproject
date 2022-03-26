569778ddda113bd396877d3e485b4e8a10dd58a7
pewphoria <101679444+pewphoria@users.noreply.github.com>
2022-03-25 21:13:51 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ let date = new Date(timestamp);
 let hours = date.getHours();
 if (hours < 10) {
   hours = '0${hours}';
+}
 let minutes = date.getMinutes();
 if (minutes < 10) {
   minutes = '0${minutes}';
