c4328573c0a3989df4ca7a03e1f227d79a36857d
pewphoria <101679444+pewphoria@users.noreply.github.com>
2022-03-29 17:02:04 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -128,6 +128,12 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
